8cda104bfdb0ed494b97af5521160bcccfeb7690
Joel Coddington <coddingtonjoel@gmail.com>
2020-04-13 00:15:19 -0700

    New Header SVG
    I did the impossible

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,9 @@ import SVG from "./SVG";
 const Header = () => {
     return (
         <div className="header">
-            <SVG />
-            <main className="header-container">
-                <h1 className="header-head">
-                    My name is Joel Coddington.
-                    <br />
-                    I'm a student web developer.
-                </h1>
-                <button className="header-btn">
-                    <span className="header-btn-span">Learn More</span>
-                </button>
-            </main>
+            <div className="header-svg">
+                <SVG />
+            </div>
         </div>
     );
 };
