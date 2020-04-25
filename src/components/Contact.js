129c45b45b5ce3ea0187205ecf8185e7937a554e
Joel Coddington <coddingtonjoel@gmail.com>
2020-04-24 23:51:21 -0700

    v1
    finished full page, more pretty stuff to come?

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,7 @@
 import React from "react";
+import linkedin from "../images/linkedin-brands.svg";
+import github from "../images/github-brands.svg";
+import codepen from "../images/codepen-brands.svg";
 
 const Contact = () => {
     return (
@@ -6,6 +9,35 @@ const Contact = () => {
             <div className="about-head-wrapper">
                 <h2 className="about-head heading">Contact</h2>
             </div>
+            <div className="contact-body">
+                <p>
+                    Feel free to email me at{" "}
+                    <span className="bold">coddingtonjoel@gmail.com</span>.
+                </p>
+                <a href="#!">Click here to view my resume!</a>
+            </div>
+            <div className="contact-buttons">
+                <a
+                    target="blank"
+                    rel="noopener noreferrer"
+                    href="https://www.linkedin.com/in/joel-coddington/">
+                    <img src={linkedin} alt="" />
+                </a>
+                <a
+                    target="blank"
+                    rel="noopener noreferrer"
+                    href="https://github.com/coddingtonjoel">
+                    <img src={github} alt="" />
+                </a>
+                <a
+                    target="blank"
+                    rel="noopener noreferrer"
+                    href="https://codepen.io/coddingtonjoel">
+                    <img src={codepen} alt="" />
+                </a>
+            </div>
+            <p className="contact-copyright">Joel Coddington ©2020</p>
+            &nbsp;
         </div>
     );
 };
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,57 +1,81 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import ProjectInfo from "./ProjectInfo";
 import proj from "../ProjectData";
+import MediaQuery from "react-responsive";
+import ProjectAccordion from "./ProjectAccordion";
 
 const Projects = () => {
     const [selected, setSelected] = useState(proj[0]);
 
     return (
-        <div className="projects" id="projects">
-            <div className="about-head-wrapper">
-                <h2 className="about-head heading">Projects</h2>
-            </div>
-            <div className="projects-container">
-                <div className="projects-container-buttons">
-                    <button
-                        href="#!"
-                        onClick={() => {
-                            setSelected(proj[0]);
-                        }}
-                        className="project-button"
-                        key={proj[0].title}
-                        autoFocus>
-                        <img src={proj[0].logo} alt="" />
-                    </button>
-                    <button
-                        href="#!"
-                        onClick={() => {
-                            setSelected(proj[1]);
-                        }}
-                        className="project-button"
-                        key={proj[1].title}>
-                        <img src={proj[1].logo} alt="" />
-                    </button>
-                    <button
-                        href="#!"
-                        onClick={() => {
-                            setSelected(proj[2]);
-                        }}
-                        className="project-button"
-                        key={proj[2].title}>
-                        <img src={proj[2].logo} alt="" />
-                    </button>
+        <Fragment>
+            <div className="projects" id="projects">
+                <div className="about-head-wrapper">
+                    <h2 className="about-head heading">Projects</h2>
                 </div>
-                <div className="projects-container-splitter" />
-                <div className="projects-container-info">
-                    <ProjectInfo
-                        title={selected.title}
-                        sc={selected.sc}
-                        desc={selected.desc}
-                        link={selected.link}
-                    />
-                </div>
+                <MediaQuery minDeviceWidth={1001}>
+                    <div className="projects-container">
+                        <div className="projects-container-buttons">
+                            <button
+                                href="#!"
+                                onClick={() => {
+                                    setSelected(proj[0]);
+                                }}
+                                className="project-button"
+                                key={proj[0].title}
+                                autoFocus>
+                                <img src={proj[0].logo} alt="" />
+                            </button>
+                            <button
+                                href="#!"
+                                onClick={() => {
+                                    setSelected(proj[1]);
+                                }}
+                                className="project-button"
+                                key={proj[1].title}>
+                                <img src={proj[1].logo} alt="" />
+                            </button>
+                            <button
+                                href="#!"
+                                onClick={() => {
+                                    setSelected(proj[2]);
+                                }}
+                                className="project-button"
+                                key={proj[2].title}>
+                                <img src={proj[2].logo} alt="" />
+                            </button>
+                        </div>
+                        <div className="projects-container-splitter" />
+                        <div className="projects-container-info">
+                            <ProjectInfo
+                                title={selected.title}
+                                sc={selected.sc}
+                                desc={selected.desc}
+                                link={selected.link}
+                            />
+                        </div>
+                    </div>
+                </MediaQuery>
+                <MediaQuery maxDeviceWidth={1000}>
+                    <div className="project-accordion-container">
+                        <ul uk-accordion="collapsible: true; animation: false; multiple: true;">
+                            {proj.map((p) => {
+                                return (
+                                    <ProjectAccordion
+                                        title={p.title}
+                                        sc={p.sc}
+                                        desc={p.desc}
+                                        link={p.link}
+                                        logo={p.logo}
+                                        key={p.title}
+                                    />
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </MediaQuery>
             </div>
-        </div>
+        </Fragment>
     );
 };
 
