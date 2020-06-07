1af869c2d490f92bc1b92fbf74b62ecba749671d
Joel Coddington <coddingtonjoel@gmail.com>
2020-06-07 13:39:56 -0700

    Update
    added GSAP

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import SVG from "./SVG";
 import SVGMobile from "./SVGMobile";
 import SVGButton from "./SVGButton";
 import MediaQuery from "react-responsive";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { gsap } from "gsap";
 
 const Header = () => {
+    const headerText = useRef();
+
+    // this allows us to access DOM elements
+    useEffect(() => {
+        // add animations on page load using useEffect()
+        gsap.from(headerText.current, {
+            opacity: 0,
+            y: 5,
+            duration: 2,
+            delay: 2,
+            ease: "power2.out",
+        });
+    }, [headerText]);
+
     return (
         <div id="home">
             <MediaQuery minDeviceWidth={701}>
@@ -14,8 +29,7 @@ const Header = () => {
                         <SVG />
                     </div>
                     <div className="header-text-wrapper">
-                        {/* TODO add GSAP animation like sketch website */}
-                        <h1 className="header-text heading">
+                        <h1 className="header-text heading" ref={headerText}>
                             My name is Joel Coddington.
                             <br />
                             I'm a student web developer.
