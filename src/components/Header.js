eecfc81abf5a0c428a8081174142f34f31ef9bea
Joel Coddington <coddingtonjoel@gmail.com>
2020-06-08 13:59:50 -0700

    Bug fixes
    removed gsap, added header text animation with pure css. fixed sizing issues for modal on different devices

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import SVG from "./SVG";
 import SVGMobile from "./SVGMobile";
 import SVGButton from "./SVGButton";
 import MediaQuery from "react-responsive";
 import AnchorLink from "react-anchor-link-smooth-scroll";
-import { gsap } from "gsap";
 
 const Header = () => {
-    const headerText = useRef();
-
-    // this allows us to access DOM elements
-    useEffect(() => {
-        // add animations on page load using useEffect()
-        gsap.from(headerText.current, {
-            opacity: 0,
-            y: 5,
-            duration: 2,
-            delay: 2,
-            ease: "power2.out",
-        });
-    }, [headerText]);
-
     return (
         <div id="home">
             <MediaQuery minDeviceWidth={701}>
@@ -29,7 +14,7 @@ const Header = () => {
                         <SVG />
                     </div>
                     <div className="header-text-wrapper">
-                        <h1 className="header-text heading" ref={headerText}>
+                        <h1 className="header-text heading">
                             My name is Joel Coddington.
                             <br />
                             I'm a student web developer.
