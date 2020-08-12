e3d88c4b0c6150b1963418fc69405d1d3de22f6d
Joel Coddington <coddingtonjoel@gmail.com>
2020-08-11 22:05:23 -0700

    Added Navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const Layout = ({ children }) => {
     return (
         <React.Fragment>
+            <Navbar />
             <main>{children}</main>
         </React.Fragment>
     );
