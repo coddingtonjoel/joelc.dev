4b09bc5006b7ec44f15cdfb350c7ab5bb7d12582
Joel Coddington <coddingtonjoel@gmail.com>
2025-10-26 17:22:59 -0700

    Replace projects image

--- src/components/index/Projects.js
+++ src/components/index/Projects.js
@@ -116,7 +116,7 @@ const Wrapper = styled.section`
 
 const query = graphql`
     {
-        file(relativePath: { eq: "projects-background.jpg" }) {
+        file(relativePath: { eq: "projects-bkg.jpg" }) {
             childImageSharp {
                 fluid(quality: 80) {
                     ...GatsbyImageSharpFluid
